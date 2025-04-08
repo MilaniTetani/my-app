055664b7eb218c6fdc395c435a2839682b0545ec
Milani <milani1tetani@gmail.com>
2025-04-08 23:38:39 +0200

    Added a studying gif

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-"use client";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Welcome to Milani's 2025 School and Study TimeTable</h1>
+    <main className="main-wrapper">
+      <div className="welcome-container">
+        <h1 className="welcome-heading">
+          Welcome to Milani's 2025 School and Study TimeTable
+        </h1>
+        <Image
+          src="/studying.gif"
+          alt="Studying Gif"
+          width={300}
+          height={300}
+          className="study-gif"
+        />
       </div>
     </main>
   );
