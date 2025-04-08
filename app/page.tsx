fe2bcb55f54f9e2ba950b93d790f1d57bedcadc6
Milani <milani1tetani@gmail.com>
2025-04-08 23:47:55 +0200

    Added an interactive button to show Timetables

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [showTimetable, setShowTimetable] = useState(false);
+
   return (
     <main className="main-wrapper">
       <div className="welcome-container">
@@ -8,12 +11,15 @@ export default function Home() {
           Welcome to Milani's 2025 School and Study TimeTable
         </h1>
         <Image
-          src="/studying.gif"
+          src="/study.gif"
           alt="Studying Gif"
           width={300}
           height={300}
           className="study-gif"
         />
+        <button className="show-btn" onClick={() => setShowTimetable(true)}>
+          Click to see!
+        </button>
       </div>
     </main>
   );
